7a09d324a2c7c9d2d2ea639763d9517b13e68ab3
willmay <williammayhew@hotmail.co.uk>
2023-03-28 20:01:15 +0100

    Initial connection between Client and Controller made

--- src/Controller.java
+++ src/Controller.java
@@ -1,2 +1,39 @@
+import java.io.*;
+import java.net.*;
+import java.util.logging.Logger;
+
 public class Controller {
+
+    static Logger logger = Logger.getLogger(Controller.class.getName());
+
+    private static int cport;
+    private static int r;
+    private static int timeout;
+    private static int rebalance;
+
+    public static void main(String[] args){
+
+        cport = Integer.parseInt(args[0]);
+        r = Integer.parseInt(args[1]);
+        timeout = Integer.parseInt(args[2]);
+        rebalance = Integer.parseInt(args[3]);
+
+        try{
+            ServerSocket ss = new ServerSocket(cport);
+            logger.info("Listening on port " + cport);
+            for(;;){
+                try{
+                    Socket client = ss.accept();
+                    logger.info("Connected");
+                    BufferedReader in = new BufferedReader(new InputStreamReader(client.getInputStream()));
+                    String line;
+                    while((line = in.readLine()) != null) {
+
+                    }
+                } catch (Exception e){System.out.println("Error " + e);}
+            }
+        }catch (Exception e){System.out.println("Error " + e);}
+
+    }
+
 }
--- src/Dstore.java
+++ src/Dstore.java
@@ -1,2 +1,17 @@
+import java.io.File;
+
 public class Dstore {
+
+    private static int port;
+    private static int cport;
+    private static int timeout;
+    private static String filePath;
+
+    public static void main(String[] args){
+        port = Integer.parseInt(args[0]);
+        cport = Integer.parseInt(args[1]);
+        timeout = Integer.parseInt(args[2]);
+        filePath = args[3];
+    }
+
 }
