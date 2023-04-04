1c2b0e26d67ca157d972c587fd0ba4242bfdb2d8
will <williammayhew@hotmail.co.uk>
2023-04-04 19:18:59 +0100

    Implemented CountDownLatch for STORE and REMOVE. Added Concurrent Operation cases

--- src/Controller.java
+++ src/Controller.java
@@ -4,6 +4,7 @@ import java.io.*;
 import java.net.*;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.CountDownLatch;
 
 public class Controller {
 
@@ -13,6 +14,8 @@ public class Controller {
     private static int r;
     private static int timeout;
     private static int rebalance;
+    private static CountDownLatch storeLatch;
+    private static CountDownLatch removeLatch;
 
     public static void main(String[] args){
 
@@ -29,6 +32,10 @@ public class Controller {
         try{
             ServerSocket ss = new ServerSocket(cport);
             System.out.println("Controller listening on port " + cport);
+
+           storeLatch = null;
+           removeLatch = null;
+
             for(;;){
                 try{
                     Socket client = ss.accept();
@@ -36,6 +43,7 @@ public class Controller {
 
                     new Thread(() -> {
                         int portIndex = 0;
+                        int totalStores = 0;
                         try{
                             BufferedReader in = new BufferedReader(new InputStreamReader(client.getInputStream()));
                             PrintWriter out = new PrintWriter(client.getOutputStream(), true);
@@ -43,7 +51,7 @@ public class Controller {
                             boolean dstore = false;
                             while((line = in.readLine()) != null) {
                                 String[] message = line.split(" ");
-                                System.out.println("Received Command: " + message[0]);
+                                System.out.println("Received Command: " + line);
 
                                 switch (message[0]){
                                     case "LIST":
@@ -52,25 +60,26 @@ public class Controller {
                                         } else{
                                             //client list
                                             if(enoughDstores()){
+                                                client.setSoTimeout(timeout);
                                                 listFiles(out);
                                             } else{
-                                                errorNotEnoughDstores(out);
+                                                Errors.errorNotEnoughDstores(out);
                                             }
                                         }
                                         break;
                                     case "STORE":
-                                        if(dstore){
-                                            //dstore store
-                                        } else{
+                                        if(!dstore){
                                             //client store
+                                            storeLatch = new CountDownLatch(r);
                                             if(enoughDstores()){
                                                 if(!index.fileExists(message[1])){
                                                     store(out, message[1], Integer.parseInt(message[2]));
+                                                    client.setSoTimeout(timeout);
                                                 } else{
-                                                    errorFileAlreadyExists(out);
+                                                    Errors.errorFileAlreadyExists(out);
                                                 }
                                             } else{
-                                                errorNotEnoughDstores(out);
+                                                Errors.errorNotEnoughDstores(out);
                                             }
                                         }
                                         break;
@@ -79,7 +88,7 @@ public class Controller {
                                         if(enoughDstores()){
                                             loadFile(out, getPort(portIndex), message[1]);
                                         } else{
-                                            errorNotEnoughDstores(out);
+                                            Errors.errorNotEnoughDstores(out);
                                         }
                                         break;
                                     case "RELOAD":
@@ -88,17 +97,23 @@ public class Controller {
                                         if(dstoreList.size() > portIndex){
                                             loadFile(out, getPort(portIndex), message[1]);
                                         } else{
-                                            out.println("ERROR_LOAD");
-                                            System.out.println("Command sent: ERROR_LOAD");
+                                            portIndex = 0;
+                                            Errors.errorLoad(out);
                                         }
                                         break;
                                     case "REMOVE":
                                         //client remove
+                                        removeLatch = new CountDownLatch(dstoreList.size());
+                                        portIndex = 0;
                                         if(enoughDstores()){
-                                            removeFile(out, message[1]);
+                                            client.setSoTimeout(timeout);
+                                            removeFile(out, message[1], getPort(dstoreList.size()-1));
+                                            portIndex++;
                                         } else{
-                                            errorNotEnoughDstores(out);
+                                            Errors.errorNotEnoughDstores(out);
                                         }
+
+
 
                                         break;
                                     case "ACK":
@@ -106,11 +121,43 @@ public class Controller {
                                         break;
                                     case "STORE_ACK":
                                         //dstore store ack
-                                        out.println("STORE_COMPLETE");
-                                        System.out.println("Command sent: STORE_COMPLETE");
+//                                        totalStores++;
+//
+//                                        if(totalStores == r){
+//                                            totalStores = 0;
+//                                            storeComplete(out, message[1]);
+//                                        }
+
+                                        synchronized (storeLatch){
+                                            storeLatch.countDown();
+                                            if(storeLatch.getCount() == 0){
+                                                System.out.println("All STORE_ACKs received");
+                                                storeComplete(out, message[1]);
+                                            }
+                                        }
+
                                         break;
                                     case "REMOVE_ACK":
                                         //dstore remove ack
+                                        if(dstoreList.size() > portIndex){
+                                            client.setSoTimeout(timeout);
+                                            removeFile(out,message[1], getPort(portIndex));
+                                            portIndex++;
+                                        } else{
+                                            removeComplete(out, message[1]);
+                                        }
+
+                                        synchronized (removeLatch){
+                                            removeLatch.countDown();
+                                            if(removeLatch.getCount() == 0){
+                                                System.out.println("All REMOVE_ACKs received");
+                                                removeComplete(out, message[1]);
+                                            } else {
+                                                client.setSoTimeout(timeout);
+                                                removeFile(out, message[1], getPort((int) (removeLatch.getCount()-1)));
+                                            }
+                                        }
+
                                         break;
                                     case "JOIN":
                                         //dstore join
@@ -126,18 +173,26 @@ public class Controller {
                                         break;
                                 }
                             }
+                            if(storeLatch != null){
+                                try{
+                                    storeLatch.await();
+                                } catch (InterruptedException e){
+                                    System.out.println("Error " + e);
+                                }
+                            }
                             client.close();
-                        }catch (Exception e){System.out.println("Error " + e);}
+                        }catch (SocketTimeoutException e){System.out.println("Socket timeout");}
+                        catch (Exception e){System.out.println("Error " + e);}
                     }).start();
-                } catch (Exception e){System.out.println("Error " + e);}
+                }catch (Exception e){System.out.println("Error " + e);}
             }
         }catch (Exception e){System.out.println("Error " + e);}
 
     }
 
     /**
-     * Checks if there is enough Dstores relative to R
-     * @return
+     * Checks if there is enough Dstores relative to r
+     * @return More than or equal to r Dstores
      */
     private static boolean enoughDstores(){
         return dstoreList.size() >= r;
@@ -151,7 +206,9 @@ public class Controller {
         String message = "LIST ";
         ArrayList<String> fileNames = index.getFileNames();
         for(String fileName : fileNames){
-            message = message + fileName + " ";
+            if(!index.fileBusy(fileName)){
+                message = message + fileName + " ";
+            }
         }
 
         out.println(message);
@@ -165,15 +222,24 @@ public class Controller {
      * @param filesize size of file to store
      */
     private static void store(PrintWriter out, String filename, int filesize){
-        index.addFile(filename,filesize);
-        System.out.println("Index Update: " + filename + " Stored");
+        if(index.fileBusy(filename)){
+            Errors.errorFileAlreadyExists(out);
+        } else{
+            index.addFile(filename,filesize);
 
-        String message = "STORE_TO ";
-        for(int i=0; i<dstoreList.size(); i++){
-            message = message + dstoreList.get(i) + " ";
+            String message = "STORE_TO ";
+            for(int i=0; i<dstoreList.size(); i++){
+                message = message + dstoreList.get(i) + " ";
+            }
+            out.println(message);
+            System.out.println("Command sent: STORE_TO");
         }
-        out.println(message);
-        System.out.println("Command sent: STORE_TO");
+    }
+
+    private static void storeComplete(PrintWriter out, String fileName){
+        index.addFileComplete(fileName);
+        out.println("STORE_COMPLETE");
+        System.out.println("Command sent: STORE_COMPLETE");
     }
 
     /**
@@ -185,8 +251,8 @@ public class Controller {
     private static void loadFile(PrintWriter out, int port, String fileName){
         int fileSize;
 
-        if(!index.fileExists(fileName)){
-            errorFileDoesNotExist(out);
+        if(!index.fileExists(fileName) || index.fileBusy(fileName)){
+            Errors.errorFileDoesNotExist(out);
         }
 
         fileSize = index.getFileSize(fileName);
@@ -199,37 +265,29 @@ public class Controller {
     }
 
     /**
-     * Removes a file with a given name from all Dstores
+     * Removes a file with a given name from a given Dstore
      * @param out Print writer
      * @param fileName File name
+     * @param port Dstore port
      */
-    private static void removeFile(PrintWriter out, String fileName){
-        if(index.fileExists(fileName)){
-            for(Integer dstorePort : dstoreList){
-                try{
-                    Socket dstoreSocket = new Socket(InetAddress.getLoopbackAddress(),dstorePort);
-                    PrintWriter dstoreWriter = new PrintWriter(dstoreSocket.getOutputStream(), true);
-                    dstoreWriter.println("REMOVE " + fileName);
-                } catch(Exception e){System.out.println("Error " + e);}
-            }
+    private static void removeFile(PrintWriter out, String fileName, int port){
+        if(index.fileExists(fileName) && !index.fileBusy(fileName)){
+            index.removeFile(fileName);
+            try{
+                Socket dstoreSocket = new Socket(InetAddress.getLoopbackAddress(),port);
+                PrintWriter dstoreWriter = new PrintWriter(dstoreSocket.getOutputStream(), true);
+                dstoreWriter.println("REMOVE " + fileName);
+                System.out.println("Command sent: REMOVE");
+            } catch(Exception e){System.out.println("Error " + e);}
         } else{
-            errorFileDoesNotExist(out);
+            Errors.errorFileDoesNotExist(out);
         }
     }
 
-    private static void errorNotEnoughDstores(PrintWriter out){
-        out.println("ERROR_NOT_ENOUGH_DSTORES");
-        System.out.println("Command sent: ERROR_NOT_ENOUGH_DSTORES");
-    }
-
-    private static void errorFileAlreadyExists(PrintWriter out){
-        out.println("ERROR_FILE_ALREADY_EXISTS");
-        System.out.println("Command sent: ERROR_FILE_ALREADY_EXISTS");
-    }
-
-    private static void errorFileDoesNotExist(PrintWriter out){
-        out.print("ERROR_FILE_DOES_NOT_EXIST");
-        System.out.println("Command sent: ERROR_FILE_DOES_NOT_EXIST");
+    private static void removeComplete(PrintWriter out, String fileName){
+        index.removeFileComplete(fileName);
+        out.println("REMOVE_COMPLETE");
+        System.out.println("Command sent: REMOVE_COMPLETE");
     }
 
     private static void rebalance(){
